0113198bbbb71fd72f380b2bef4329f41307d3ed
忏 <202313093011@cuc.edu.cn>
2025-03-02 23:06:38 +0800

    试试

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,21 @@
 import type { AstroIntegration } from "@swup/astro";
 
 declare global {
+  // Remove twikoo import error
   interface Window {
     swup: AstroIntegration;
   }
 }
+
+declare module "twikoo" {
+  // 声明默认导出为一个包含 init 方法的对象
+  const twikoo: {
+    init: (options: {
+      envId: string;
+      region?: string;
+      el?: string;
+    }) => Promise<void>;
+  };
+  export default twikoo;
+}
+
